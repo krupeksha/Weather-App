65a95fca8ff74a720166b45e22b23915ae2e08ae
krupeksha <krupeksha1234@gmail.com>
2023-06-16 16:24:21 +0530

    project for weather

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { WeatherService } from './weather.service';
 
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-app';
+  IsChangeLocation=false;
+  TodayDate: Date;
+  weatherData: any;
+  cityName: any;
+
+  constructor(private service:WeatherService){
+    this.TodayDate = new Date();
+  }
+
+  changeLocation(){
+    debugger
+    this.IsChangeLocation = this.IsChangeLocation==true?false:true;
+  }
+
+  getWeatherDataByCity(){
+    debugger
+    var city = $("#CityName").val();
+    this.service.getWeatherData(city).subscribe(data=>{
+      this.cityName = city;
+      console.log('data',data)
+      this.weatherData = data;
+    })
+  }
+  transform(value: number): number {
+    return Math.round(value);
+  }
 }
---
+++ src/app/weather.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  constructor(private http: HttpClient) { }
+  getWeatherData(city:string):Observable<any>{
+    var  headers={
+      'X-RapidAPI-Key': '3751411290msh82dbe512d14a17fp1ea402jsn78b97423211a',
+      'X-RapidAPI-Host': 'openweather43.p.rapidapi.com'
+    }
+    var q = city;
+    var appid = ['da0f9c8d90bde7e619c3ec47766a42f4'];
+    var units = 'standard';
+      return this.http.get('https://openweather43.p.rapidapi.com/weather',{params:{q,appid,units },headers:headers})
+  }
+  
+
+}
